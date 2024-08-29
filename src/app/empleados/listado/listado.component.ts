e72ba3ea635e072f1ba3d06859af798d166b6556
mama679 <ingmariomanotas@hotmail.com>
2024-08-28 23:08:36 -0500

    Listado componente, con datos API

--- src/app/empleados/listado/listado.component.ts
+++ src/app/empleados/listado/listado.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmpleadoService } from '../empleado.service';
+import { Empleado } from '../model/empleado';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
 
+   empleados:Empleado[] = [];
+   displayedColumns:string[] = ['id','name','username','email','phone','website']
+  constructor(private servicio:EmpleadoService) {
+    
+  }
+
+  ngOnInit(): void {
+      this.obtLista();
+  }
+
+  obtLista(){
+    this.servicio.listarEmpleados().subscribe((resp:any)=>{
+      if(!resp.error && resp){
+        this.empleados = resp;
+      }
+      else{
+        console.log(resp);
+      }
+    });
+  }
 }
